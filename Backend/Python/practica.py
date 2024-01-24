d16fd0ea1cbae5e2efce41d1723e87688be4224d
luisftroncoso2010 <troncober@hotmail.com>
2024-01-24 10:39:50 -0500

    completar flora HTML

--- Backend/Python/practica.py
+++ Backend/Python/practica.py
@@ -2,16 +2,21 @@ import random as rn
 
 
 print('-- Generar una lista de numeros con el el modulo randim')
+
+
 numeros = [rn.randint(1, 10) for _ in range(10)]
 print(numeros)
 
 
 print('-- Crear una lista con la funcion range()')
+
+
 numeros_dos = list(range(1, 10))
 print(numeros_dos)
 
 
 print('-- Creamos una tabla de multiplicar con la funcion range()')
+
 
 for i in range(1, 11):
     print(f'Tabla de multiplicar {i}')
