25ff5c94dfbab2572b2edb06cdd2ddd680c6cc81
luisftroncoso2010 <troncober@hotmail.com>
2024-01-26 10:34:56 -0500

    Cambio de nombres(letrar) por numeros

---
+++ Backend/Python/Practicas/cambio_de _letras_palabras.py
@@ -0,0 +1,39 @@
+'''Cambio de letras por palabras.
+Se desea cambiar el nombre que tiene una lista de nombres'''
+
+print('-- Cambio de letras por numeros en nombre.')
+
+
+# Creamos el set e nombres
+nombres = ["JUAN", "EVELIN", "ANA", "MARK", "DIEGO", "ABCD", "ZY"]
+# Creamos el sets de abecedario
+abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+              'H', 'I', 'J', 'K', 'L', 'M', 'N',
+              'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T',
+              'U', 'V', 'W', 'X', 'Y', 'Z']
+# Creamos el set de numeros de remplazo (Numero iguales que el abecedario)
+numerosABC = list(range(1, 28))
+
+
+# Iniciamos la variable para con tar el ciclo
+x = 0
+# Creamoa la variable auxiliar. Amacenaremos los que ya van
+nombreAuxiliar = ''
+
+# Contamos los elementos del set nombre, para iniciar el ciclo
+NUMERODEELEMENTOS = len(nombres)
+
+
+# Iniciamos el ciclo con el valor del set nombres
+while x < NUMERODEELEMENTOS:
+    # Iteramos sobre los caracteres de los elementos del set nombres
+    for letra in nombres[x]:
+        # Iteramos sobre el set Abecedario
+        for indice, letra_abecedario in enumerate(abecedario):
+            # Condicion para comparar cada caracter de los elementos
+            # Del set abecedario
+            if letra == letra_abecedario:
+                nombreAuxiliar += str(numerosABC[indice])
+    print(f'Nombre: {nombres[x]}\tNumero: {nombreAuxiliar}')
+    nombreAuxiliar = ''
+    x += 1
