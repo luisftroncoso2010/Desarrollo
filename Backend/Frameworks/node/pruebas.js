de2395aab2a41b1cb97ff7cd02647c4dd6eddf14
luisftroncoso2010 <troncober@hotmail.com>
2024-05-02 21:56:30 -0500

    Trabajo frontend

---
+++ Backend/Frameworks/node/pruebas.js
@@ -0,0 +1,8 @@
+format.addEventListener('onsubmit', function(even){
+
+})
+¿Cual es la que mejro se ajusta la codigo?
+
+se produce un error.
+se envían los datos ingresados en form.
+Ninguna de las dos opciones es correcta.
