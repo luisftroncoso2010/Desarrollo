0d2696b8fc4639aa11464dd8c60d6471a4e5cadf
luisftroncoso2010 <troncober@hotmail.com>
2024-06-19 17:40:28 -0500

    Enrutamiento React, Hystory Api (useNavigate)

--- FrontEnd/React/Digital_House/clase_once/practica/src/App.jsx
+++ FrontEnd/React/Digital_House/clase_once/practica/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import  Navbar  from './Componentes/Navbar'
 import  Footer  from './Componentes/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 function App() { 
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/Home')
+  }
 
   return (
     <>      
       <Navbar/> 
       <Outlet /> 
-      <Footer />    
+      <Footer /> 
+      <br />
+      <button onClick={handleClick}> 
+      App
+      </button>   
+
     </>
   )
 }
--- FrontEnd/React/Digital_House/clase_once/practica/src/Componentes/Home.jsx
+++ FrontEnd/React/Digital_House/clase_once/practica/src/Componentes/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const navigate = useNavigate()
+
   return (
     <div>
       Estas en el Home
+      <br />
+      <button onClick={() => navigate(-1)}> Go back</button>
     </div>
   )
 }
