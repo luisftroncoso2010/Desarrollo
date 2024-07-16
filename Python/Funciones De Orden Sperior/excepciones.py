0b51d857d05bfc00d8497f0241aeeb90f812b70a
luisftroncoso2010 <troncober@hotmail.com>
2024-07-16 17:37:10 -0500

    Python para todos: Tecnicas avanadas de formateo

--- Python/Funciones De Orden Sperior/excepciones.py
+++ Python/Funciones De Orden Sperior/excepciones.py
@@ -1,8 +1,11 @@
 """ Excepciones python """
 
 print(' * Divicion entre 0: ')
+
+
 def divicion(a, b):
     return a / b
+
 
 try:
     resultado = divicion(1, 0)
@@ -19,11 +22,11 @@ except NameError:
     print('La variable no existe')
 except ValueError:
     print('El valor no es un numero')
-else: 
-    print('Bloque else: Solo se ejecuta si no a ocurrido una excepcion en el try')
+else:
+    print('Bloque else: Solo se ejecuta si no a ocurrido una excepcion')
 finally:
     print('Bloque finally: Este se ejecuta siempre')
-    
+
 
 print(' * Crear nuestras propias excepciones: ')
 
@@ -31,15 +34,15 @@ print(' * Crear nuestras propias excepciones: ')
 class MiError(Exception):
     def __init__(self, valor):
         self.valor = valor
-        
+
     def __str__(self):
         return print(f'Error: {str(self.valor)}')
 
-resultado = 30 
+
+resultado = 30
 # Ejemplo de uso
 try:
     if resultado > 20:
         raise MiError(33)
 except MiError as e:
     print(e)
-        
---
+++ Python/Funciones De Orden Sperior/salida_estandar.py
@@ -0,0 +1,9 @@
+from math import pi
+
+
+print("%10s mundo" % "Hola")
+print("%-10s mundo" % "Hola")
+
+
+print("%.4f" % pi)
+print("%.4s" % "Hola mundo")
