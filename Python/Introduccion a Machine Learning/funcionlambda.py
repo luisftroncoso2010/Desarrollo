9f02970ff6ccbce7a8a64102807ad75cbee2020b
luisftroncoso2010 <troncober@hotmail.com>
2024-08-14 17:00:56 -0500

    Introducción a Machine Learning, fuciones lambda

---
+++ Python/Introduccion a Machine Learning/funcionlambda.py
@@ -0,0 +1,30 @@
+"""Es una función anónima"""
+
+
+def doble(x):
+    return x * 2
+
+
+print(doble(4))
+
+
+# Doble con funcion lambda
+
+
+dobleDos = lambda x: x * 2
+print(dobleDos(2))
+
+
+# Lista con funcion lambda y filter
+lista = [1, 2, 44, 48, 4, 24, 12, 68, 67]
+listaPares = list(filter(dobleDos, lista))
+print(listaPares)
+
+# Lambda interno
+listaParesDos = list(filter(lambda x: x % 2 == 0, lista))
+print(listaParesDos)
+
+# Elementos en una lista. Duplicarlos
+listaDos = [ 4, 8, 7, 69, 4, 20, 10, 11]
+listaDoble = list(map(lambda x: x * 2, listaDos))
+print(listaDoble)
