c727a6c1c1c05e304768820c32394b6cd355cb4d
luisftroncoso2010 <troncober@hotmail.com>
2024-08-14 16:00:48 -0500

    Introducción a Machine Learning, funcion range

---
+++ Python/Introduccion a Machine Learning/funcionRange.py
@@ -0,0 +1,34 @@
+"""Funcion range genera números en el enteros antre el final y 
+el entero final"""
+
+# Genera una lista de numéros a partir de una lista
+print(list(range(0, 7)))
+
+# Mostrar numéros del 0 a 7
+for i in range(0, 7):
+    print(i, end=', ')
+
+# Imprimir con saltos
+print('\n')
+for i in range(2, 10, 2):
+    print(i, end=', ')
+
+# Colocar con numeros
+print('\n')
+for i in range(2, 10, 2):
+    print('El valor de i es: ', i)
+
+# Generar un triangulo
+for num in range(10):
+    for i in range(num):
+        print(num, end=' ')
+    print()
+
+# Invertir un rango
+for i in reversed(range(0, 10)):
+    print(i, end=', ')
+
+# Generar lista con range
+print('\n')
+generarLista = list(range(1, 8))
+print('Lista: ', generarLista)
