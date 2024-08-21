84bc1137aa01ed14c709064db3fc862c540bdefa
luisftroncoso2010 <troncober@hotmail.com>
2024-08-20 20:41:06 -0500

    JavaScript. Loops: for in

--- FrontEnd/JavaScript/JavaScript/ciclos/opearadores/loops_fo.js
+++ FrontEnd/JavaScript/JavaScript/ciclos/opearadores/loops_fo.js
@@ -7,7 +7,28 @@ for (let index = 0; index < list.length; index++) {
 }
 
 console.log("   * Loop: for of");
-
+/* Se usa cuento hay objetos iterables */
 for (const element of list) {
     console.log(element);   
 }
+
+console.log("   * Loop: for of");
+
+/* Creamos un objeto */
+const listaDeCompras = {
+    manzanas: 5,
+    pera: 3,
+    naranja: 2,
+    uva: 1
+}
+
+for (const key in list) {
+    console.log(`Key del Objeto: ${key}. Este es el valor: ${list[key]}`);    
+}
+
+console.log('Lista de compras: ');
+for (const key in listaDeCompras) {
+    console.log(`Key lista de Compras: ${key}. Los elementos son: ${listaDeCompras[key]}`);
+}
+
+
