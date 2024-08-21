aae101deee61bfb6a08ef580bfe88c3a63c5c26e
luisftroncoso2010 <troncober@hotmail.com>
2024-08-20 19:52:11 -0500

    JavaScript. Estructuras de control if

--- FrontEnd/JavaScript/JavaScript/ciclos/opearadores/estructuras_de_control.js
+++ FrontEnd/JavaScript/JavaScript/ciclos/opearadores/estructuras_de_control.js
@@ -1,10 +1,19 @@
 let nombre = 'Nico'
 
-if (nombre === 'Diego') {
-    console.log('Hola Diego');   
-} else if (nombre === 'Nico'){
-    console.log("Hola nico");
-} else{
-    console.log(" Nombre no encontrado");    
+if (nombre === 'Diego') console.log('Hola Diego');   
+else if (nombre === 'Nico') console.log("Hola nico");
+else console.log(" Nombre no encontrado");   
+
+console.log(" Juego: ");
+
+const numeroSecreto = Math.floor(Math.random() * 10 + 1)
+const numeroJugador = parseInt(prompt('Adivina el número secreto entre le 1 al 10 : '))
+
+console.log(`Este es el número con el que juegs: ${numeroJugador}`);
+if(numeroJugador === numeroSecreto)console.log('!Felecidades, adivinaste el número secreto¡');
+else if (numeroJugador < numeroSecreto) console.log("El número es demasido bajo, intenta de nuevo");
+else console.log("El número es muy alto, intenta de nuevo");
+
+
+ 
     
-}
