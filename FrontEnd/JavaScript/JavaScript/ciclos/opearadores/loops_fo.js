9f6f330645702e0ea4e4888e52eb9ae5b3b66206
luisftroncoso2010 <troncober@hotmail.com>
2024-08-20 20:17:25 -0500

    JavaScript. Loops: for of

--- FrontEnd/JavaScript/JavaScript/ciclos/opearadores/loops_fo.js
+++ FrontEnd/JavaScript/JavaScript/ciclos/opearadores/loops_fo.js
@@ -1,5 +1,13 @@
+console.log("   * Loop: for");
+
 let list = ['eat', 'sleep', 'code', 'repeat']
 
 for (let index = 0; index < list.length; index++) {
     console.log(list[index]);
+}
+
+console.log("   * Loop: for of");
+
+for (const element of list) {
+    console.log(element);   
 }
