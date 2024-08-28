fce4dfc7e12bb9e6ee661d30f306c76068a5b557
luisftroncoso2010 <troncober@hotmail.com>
2024-08-28 14:03:21 -0500

    JavaScript POO. Creación de Objetos, funcion constructora y por Clases

---
+++ FrontEnd/JavaScript/JavaScript/POO/clases.js
@@ -0,0 +1,17 @@
+/* Creación de clases */
+class Persona{
+    constructor(nombre, edad){
+        this.nombre = nombre,
+        this.edad = edad
+    }
+    saludar(){
+        console.log(`Hola. Mi nombre es ${this.nombre} y tengo ${this.edad}`);
+    }
+}
+
+/* Creamoa el objeto de tipo Persona */
+const personaUno = new Persona('Luis', 27)
+console.log(personaUno);
+/* Imprimimos la funcion atraves del objeto, saludar. */
+personaUno.saludar()
+
