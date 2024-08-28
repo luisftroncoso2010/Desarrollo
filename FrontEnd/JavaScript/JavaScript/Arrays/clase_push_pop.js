2a2ee2a894ab3be977c4d0ec761c29d5b7aee970
luisftroncoso2010 <troncober@hotmail.com>
2024-08-27 20:12:32 -0500

    JavaScript, Pop, Push

---
+++ FrontEnd/JavaScript/JavaScript/Arrays/clase_push_pop.js
@@ -0,0 +1,17 @@
+// Methods that modify the original array (Mutability)
+
+// Push()
+const conuntries = ['USA', 'Canada', 'UK']
+const newCountries = conuntries.push('Germany', 'Australia')
+
+console.log(conuntries);
+console.log(newCountries);
+
+
+// Pop()
+const removeCountry = conuntries.pop()
+console.log(conuntries);
+console.log(removeCountry);  // Muestra el elemento removido
+
+
+
