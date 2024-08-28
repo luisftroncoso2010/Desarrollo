4f6f2e4b2d7bf7881297584a67927b6286ef5aee
luisftroncoso2010 <troncober@hotmail.com>
2024-08-28 12:28:45 -0500

    JavaScript, find, findIndex. (No modifican el array original)

---
+++ FrontEnd/JavaScript/JavaScript/Arrays/find_findIndex.js
@@ -0,0 +1,21 @@
+// Methods that iterate over an array
+// Methods that DOT NOt modify the array original array (Inmutability)
+
+/* find() */
+const multiple0f5 = [5, 10, 15, 20]
+
+/* Numeros mayores de 10 */
+const firstNumberGreaterThan10 = multiple0f5.find(number => number > 10)
+console.log(multiple0f5);
+console.log(firstNumberGreaterThan10);
+
+
+// findIndex()
+const randomNumbers = [6, 14, 27, 56, 40]
+/* Trae el  */
+const indexNumber = randomNumbers.findIndex(number => number > 50)
+console.log(randomNumbers);
+console.log(indexNumber);
+
+
+
