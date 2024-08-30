efedf76022d52c0f777e5e74b020d9f7f403c9c7
luisftroncoso2010 <troncober@hotmail.com>
2024-08-29 20:33:21 -0500

    React. useState, eventos

--- FrontEnd/React/React/Pildoras/segunda-app/src/App.js
+++ FrontEnd/React/React/Pildoras/segunda-app/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import { useState } from 'react';
 
 /* Esto es un componente */
 function App() {
   
-  
+  /* Definir el estado para alamacenar el resultado */
+  const [resultado, setResultado] = useState(null)  
   const elementoDos = suma(5, 7)
+
+  const botonPulsado = () => {    
+    setResultado(elementoDos)
+  }
   
   return (
     <div className='centrar-titulo'>
+        <button onClick={botonPulsado}>Púlsame</button>
         <h1 >Hola mundo</h1>
-        <p>{elementoDos}</p> 
+        {/* Solo me mostrará el texto siempre que haya valor en resultado */}
+        <div>{resultado != null && <h2>El resultado es: {resultado}</h2>}</div> 
     </div>
   )
 }
