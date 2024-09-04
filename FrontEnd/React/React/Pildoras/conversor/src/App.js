9a4f0c404ca371d516bee18be2510c8699d58cc8
luisftroncoso2010 <troncober@hotmail.com>
2024-09-04 11:42:00 -0500

    React. Proyecto, Practica 'Conversor con API, peticiones Asicronas

--- FrontEnd/React/React/Pildoras/conversor/src/App.js
+++ FrontEnd/React/React/Pildoras/conversor/src/App.js
@@ -1,22 +1,47 @@
 
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  /*Al macenamos la tasa de cambio */
+  const [valorCambio, setValorCambio] = useState(null);
 
   const euroRef = useRef();
   const resultadoRef = useRef();
 
+  /* Hacemos operación asincrona. Usea effect. Llamar la api, hacer solicitudes http */
+  useEffect(() => {
+    const llamaApiCambio = async() => {
+       /* Ejecutamos el lboque try-catch para valida la tarea */
+      try {
+        const respuesta = await fetch('https://v6.exchangerate-api.com/v6/4cc111268081b395eb09468f/latest/USD')
+        const datos = await respuesta.json()
+
+        /* Muestra el Json por consola */
+        console.log(datos);
+        
+        /* Accesedemos a la tasa de valor cambio */
+        setValorCambio(datos.conversion_rates.USD)
+        
+      } catch (error) {
+        /* Dado el caso que no traiga nada o haya error mostrará este mensaje */
+        console.log('Error al acceder a la API: ', error)
+      }
+    }
+    llamaApiCambio()   
+  }, [])
+
   const calcular = () => {
     const eurosValor = parseFloat(euroRef.current.value)
-    const dolares = eurosValor * 1.11
+    const dolares = eurosValor * valorCambio
 
     resultadoRef.current.innerHTML =  '$' + dolares.toFixed(2)
   }
-
-  console.log(resultadoRef);
+  // console.log(euroRef);
+  // console.log(resultadoRef);
+  // console.log(valorCambio);
+  // console.log(setValorCambio);
   
-
   return (
     <div className="App">
       
