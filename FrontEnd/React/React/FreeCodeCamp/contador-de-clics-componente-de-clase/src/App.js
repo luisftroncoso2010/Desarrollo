c16675a2652255eecd54174aa5ee8ad9c0cad3d0
luisftroncoso2010 <troncober@hotmail.com>
2024-09-25 20:23:40 -0500

    React. Proyecto. Contador de clics adaptado a Componentes de clase

---
+++ FrontEnd/React/React/FreeCodeCamp/contador-de-clics-componente-de-clase/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+import Boton from './componentes/boton';
+import Contador from './componentes/Contador';
+import logo from './imagenes/freecodecamp-logo.png'
+
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {       
+      numClics: 0
+    };
+    this.manejarClic = this.manejarClic.bind(this)
+    this.reiniciarContador = this.reiniciarContador.bind(this)
+  }
+
+  manejarClic(){
+    this.setState(({ numClics }) => ({numClics: numClics + 1}))
+  }
+
+  reiniciarContador(){
+    this.setState({ numClics: 0})
+  }
+
+  render(){
+    return(
+      <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='logo'
+          src={logo}
+          alt='Logo de Pagina'
+          />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={this.state.numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={this.manejarClic} />
+        
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={this.reiniciarContador} />
+      </div>
+    </div>
+
+    )
+
+    
+  }
+}
+
+export default App;
